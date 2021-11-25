fe4d7dc1626b85dfd8895bb1b3dccb59b05ae318
Nagendra Mantina <venkata.m@servify.in>
2021-11-25 15:18:50 +0530

    file format to match the requirement

--- index.js
+++ index.js
@@ -1,27 +1,45 @@
 const xml=require('xml');
 const fs=require('fs');
-var example4 = [ 
- { 
-  toys: [ 
-   { _attr: { decade: '80s', locale: 'US'} }, 
-   { toy: 'Transformers' } , 
-   { toy: 'GI Joe' }, 
-   { toy: 'He-man' } 
-  ],
+
+const data=[
+ {
+  policyID: 1,
+  policyHolderName: 'Nagendra',
+  validity: 'lifetime'
  },
  {
-  cars: [
-   { 
-    sedan: [
-     {model: 'swift'},
-     {model: 'amaze' }
-    ]
-   }, 
-   { xuv: 'mahindra700' } 
-  ]
- } 
+  policyID: 2,
+  policyHolderName: 'Venkata',
+  validity: 'lifetime'
+ },
+ {
+  policyID: 3,
+  policyHolderName: 'Mantina',
+  validity: 'lifetime'
+ }
 ];
-var xmlContnet=xml(example4, { declaration: true, indent: true});
+
+const xml_data=[
+ {
+  Message:[
+   {_attr: {from: 'Samsung', sent:'20-20-21', sequenceId:1}},
+   {
+    contractInsurances:[
+
+    ]
+   }
+  ]
+ }
+];
+data.forEach(d=>{
+ const contractInsurance=[];
+ Object.keys(d).forEach(k=>{
+  contractInsurance.push({[k]:d[k]});
+ });
+ xml_data[0].Message[1].contractInsurances.push({contractInsurance});
+})
+
+var xmlContnet=xml(xml_data, { declaration: true, indent: true});
 console.log(xmlContnet);
 fs.writeFile('samplefile.xml',xmlContnet,(e,r)=>{
  if(e) return console.error(e);
